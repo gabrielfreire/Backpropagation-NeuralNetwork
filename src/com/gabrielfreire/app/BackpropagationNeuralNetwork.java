3176016c32bb249cbf0a2fc7b99de05ecfc502c7
Gabriel Freire <Gabriel Freire>
2017-04-10 17:31:43 +0100

    Backpropagation class and Layer class implementation

--- src/com/gabrielfreire/app/BackpropagationNeuralNetwork.java
+++ src/com/gabrielfreire/app/BackpropagationNeuralNetwork.java
@@ -4,4 +4,36 @@ package com.gabrielfreire.app;
  * Created by Gabriel Freire on 10/04/2017.
  */
 public class BackpropagationNeuralNetwork {
+
+    private Layer[] layers;
+
+    public BackpropagationNeuralNetwork(int inputSize, int hiddenSize, int outputSize){
+        layers = new Layer[2];
+        layers[0] = new Layer(inputSize, hiddenSize);
+        layers[1] = new Layer(hiddenSize, outputSize);
+    }
+
+    public Layer getLayer(int index){
+        return layers[index];
+    }
+
+    public float[] run(float[] input){
+        float[] activations = input;
+        for(int i = 0; i<layers.length; i++){
+            activations = layers[i].run(activations);
+        }
+        return activations;
+    }
+
+    public void train(float[] input, float[] targetOutput, float learningRate, float momentum){
+        float[] calculatedOutput = run(input);
+        float[] error = new float[calculatedOutput.length];
+
+        for(int i = 0; i < error.length; i++){
+            error[i] = targetOutput[i] - calculatedOutput[i];
+        }
+        for(int i = layers.length-1; i >= 0; i--){
+            error = layers[i].train(error, learningRate, momentum);
+        }
+    }
 }
--- src/com/gabrielfreire/app/Layer.java
+++ src/com/gabrielfreire/app/Layer.java
@@ -1,7 +1,65 @@
 package com.gabrielfreire.app;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * Created by Gabriel Freire on 10/04/2017.
  */
 public class Layer {
+
+    private float[] input;
+    private float[] output;
+    private float[] weights;
+    private float[] dWeights;
+    private Random random;
+
+    public Layer(int inputSize, int outputSize) {
+        this.output = new float[outputSize];
+        this.input = new float[inputSize+1]; // + 1 for bias node
+        this.weights = new float[(inputSize+1) * outputSize];
+        this.dWeights = new float[weights.length];
+        this.random = new Random();
+        
+        initWeights();
+    }
+
+    private void initWeights() {
+        for(int i = 0; i < weights.length; i++){
+            weights[i] = (random.nextFloat() - 0.5f) * 4f; // [-2, 2]
+        }
+    }
+
+    public float[] run(float[] inputArray) {
+        System.arraycopy(inputArray, 0, input, 0, inputArray.length);
+
+        input[input.length-1] = 1;
+        int offset = 0;
+        for(int i = 0; i < output.length; i++){
+            for(int j = 0; j < input.length; j++){
+                output[i] += weights[offset+j] * input[j];
+            }
+            output[i] = ActivationFunction.sigmoid(output[i]);
+            offset += input.length;
+        }
+        return Arrays.copyOf(output, output.length);
+    }
+
+    public float[] train(float[] error, float learningRate, float momentum) {
+        int offset = 0;
+        float[] nextError = new float[input.length];
+
+        for(int i = 0; i < output.length; i++){
+            float delta = error[i] * ActivationFunction.dSigmoid(output[i]);
+            for(int j = 0; j < input.length; j++){
+                int weightIndex = offset + j;
+                nextError[j] += weights[weightIndex] * delta;
+                float dw = input[j] * delta * learningRate;
+                weights[weightIndex] += dWeights[weightIndex] * momentum + dw;
+                dWeights[weightIndex] = dw;
+            }
+            offset += input.length;
+        }
+        return nextError;
+    }
 }
