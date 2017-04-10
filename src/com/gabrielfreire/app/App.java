6796bf646c8c7467ab046e85c8f0bc7f0922c35c
Gabriel Freire <Gabriel Freire>
2017-04-10 17:45:19 +0100

    Testing the neural network with 1000 iterations

--- src/com/gabrielfreire/app/App.java
+++ src/com/gabrielfreire/app/App.java
@@ -4,4 +4,28 @@ package com.gabrielfreire.app;
  * Created by Gabriel Freire on 10/04/2017.
  */
 public class App {
+
+    public static void main(String[] args) {
+        //test code
+        float[][] trainingData = new float[][]{
+                new float[]{0, 0},
+                new float[]{0, 1},
+                new float[]{1, 0},
+                new float[]{1, 1},
+        };
+        float[][] trainingResults = new float[][]{new float[]{0}, new float[]{0}, new float[]{0}, new float[]{1}};
+        BackpropagationNeuralNetwork backpropagationNeuralNetwork = new BackpropagationNeuralNetwork(2, 3, 1);
+
+        for(int iteration = 0; iteration < NeuralNetConstants.ITERATIONS; iteration++){
+            for(int i = 0; i < trainingResults.length; i++){
+                backpropagationNeuralNetwork.train(trainingData[i], trainingResults[i], NeuralNetConstants.LEARNING_RATE, NeuralNetConstants.MOMENTUM);
+            }
+            System.out.println();
+            for(int i = 0; i < trainingResults.length; i++){
+                float[] t = trainingData[i];
+                System.out.println("Num of iterations: " + (iteration + 1));
+                System.out.printf("%.1f, %.1f -->  %.3f\n", t[0], t[1], backpropagationNeuralNetwork.run(t)[0]);
+            }
+        }
+    }
 }
--- src/com/gabrielfreire/app/NeuralNetConstants.java
+++ src/com/gabrielfreire/app/NeuralNetConstants.java
@@ -9,6 +9,6 @@ public class NeuralNetConstants {
 
     public static final float LEARNING_RATE = 0.3f;
     public static final float MOMENTUM = 0.6f;
-    public static final int ITERATIONS = 100000;
+    public static final int ITERATIONS = 10000;
 
 }
