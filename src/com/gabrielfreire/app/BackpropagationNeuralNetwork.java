7576eaf86d0ed528ee5863aae3484504c20fc8a9
Gabriel Freire <Gabriel Freire>
2017-04-10 14:08:52 +0100

    Initial commit

---
+++ src/com/gabrielfreire/app/BackpropagationNeuralNetwork.java
@@ -0,0 +1,7 @@
+package com.gabrielfreire.app;
+
+/**
+ * Created by Gabriel Freire on 10/04/2017.
+ */
+public class BackpropagationNeuralNetwork {
+}
---
+++ src/com/gabrielfreire/app/NeuralNetConstants.java
@@ -0,0 +1,14 @@
+package com.gabrielfreire.app;
+
+/**
+ * Created by Gabriel Freire on 10/04/2017.
+ */
+public class NeuralNetConstants {
+
+    private NeuralNetConstants(){}
+
+    public static final float LEARNING_RATE = 0.3f;
+    public static final float MOMENTUM = 0.6f;
+    public static final int ITERATIONS = 100000;
+
+}
