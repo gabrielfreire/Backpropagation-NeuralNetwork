079d2dec527f54a659f7136e2ac71e5c73d6da6f
Gabriel Freire <Gabriel Freire>
2017-04-10 14:13:13 +0100

    Sigmoid and derivative Sigmoid functions implementation

--- src/com/gabrielfreire/app/ActivationFunction.java
+++ src/com/gabrielfreire/app/ActivationFunction.java
@@ -4,4 +4,13 @@ package com.gabrielfreire.app;
  * Created by Gabriel Freire on 10/04/2017.
  */
 public class ActivationFunction {
+
+    public static float sigmoid(float x){
+        return (float) (1 / (1+Math.exp(-x)));
+    }
+    //derivative sigmoid
+    //no need to call the sigmoid method again since we will only call the derivative after calling sigmoid function
+    public static float dSigmoid(float x){
+        return x * (1-x);
+    }
 }
