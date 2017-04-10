d81fbdc1c0147649b1fe1130205e7713bb853ae7
Gabriel Freire <Gabriel Freire>
2017-04-10 17:52:24 +0100

    Adding training data for the XOR problem

--- src/com/gabrielfreire/app/App.java
+++ src/com/gabrielfreire/app/App.java
@@ -15,14 +15,15 @@ public class App {
         };
         float[][] trainingResultsOR = new float[][]{new float[]{0}, new float[]{1}, new float[]{1}, new float[]{1}}; //OR
         float[][] trainingResultsAND = new float[][]{new float[]{0}, new float[]{0}, new float[]{0}, new float[]{1}}; //AND
+        float[][] trainingResultsXOR = new float[][]{new float[]{0}, new float[]{1}, new float[]{1}, new float[]{0}}; //XOR
         BackpropagationNeuralNetwork backpropagationNeuralNetwork = new BackpropagationNeuralNetwork(2, 3, 1);
 
         for(int iteration = 0; iteration < NeuralNetConstants.ITERATIONS; iteration++){
-            for(int i = 0; i < trainingResultsOR.length; i++){
-                backpropagationNeuralNetwork.train(trainingData[i], trainingResultsOR[i], NeuralNetConstants.LEARNING_RATE, NeuralNetConstants.MOMENTUM);
+            for(int i = 0; i < trainingResultsXOR.length; i++){
+                backpropagationNeuralNetwork.train(trainingData[i], trainingResultsXOR[i], NeuralNetConstants.LEARNING_RATE, NeuralNetConstants.MOMENTUM);
             }
             System.out.println();
-            for(int i = 0; i < trainingResultsOR.length; i++){
+            for(int i = 0; i < trainingResultsXOR.length; i++){
                 float[] t = trainingData[i];
                 System.out.println("Num of iterations: " + (iteration + 1));
                 System.out.printf("%.1f, %.1f --> %.3f\n", t[0], t[1], backpropagationNeuralNetwork.run(t)[0]);
